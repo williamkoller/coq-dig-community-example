afce5e5e19eb9677b2ec2efdad66e7c63faddcd5
william koller <wkoller25@gmail.com>
2021-08-27 02:13:40 -0300

    chore: resolve path absolute

--- src/modules/cats/controllers/cats.controllers.ts
+++ src/modules/cats/controllers/cats.controllers.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { CreateCatDto } from '@/modules/cats/dtos/create-cat/create-cat.dto';
 import { CreateCatService } from '@/modules/cats/services/create-cat/create-cat.service';
 import { Cat } from '@/modules/cats/types/cat/cat.type';
 import { ListCatsService } from '@/modules/cats/services/list-cats/list-cats.service';
-import { DeleteCatService } from '../services/delete-cat/delete-cat.service';
+import { DeleteCatService } from '@/modules/cats/services/delete-cat/delete-cat.service';
 
 @Controller('cats')
 export class CatsController {
@@ -14,16 +23,20 @@ export class CatsController {
   ) {}
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   public create(@Body() createCatDto: CreateCatDto): Cat {
     return this.createCatService.create(createCatDto);
   }
 
   @Get()
+  @HttpCode(HttpStatus.OK)
   public index(): Cat[] {
     return this.listCatsService.listCats();
   }
 
   @Delete(':id')
+  @Get()
+  @HttpCode(HttpStatus.OK)
   public delete(@Param('id') id: number): { message: string } {
     return this.deleteCatService.deleteCat(id);
   }
--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -1,13 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUserDto } from '../dtos/create-user/create-user.dto';
-import { CreateUserService } from '../services/create-user/create-user.service';
-import { User } from '../types/user/user.type';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { CreateUserDto } from '@/modules/users/dtos/create-user/create-user.dto';
+import { CreateUserService } from '@/modules/users/services/create-user/create-user.service';
+import { User } from '@/modules/users/types/user/user.type';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly createUserService: CreateUserService) {}
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   public async create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.createUserService.create(createUserDto);
   }
